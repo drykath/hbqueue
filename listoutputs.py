65a515cbe47d8bb4964cdf918293c9180333fe94
Drykath <drykath@drykath.com>
2022-10-09 09:09:41 -0400

    Fancy up listoutputs
    Now checks for each output file and displays whether it sees it at the
    destination path.

--- listoutputs.py
+++ listoutputs.py
@@ -6,14 +6,27 @@ import sys
 
 qfile = sys.argv[1]
 
+concolor = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'end': '\033[0m',
+    }
+
 with open(qfile) as f:
     # List of jobs in queue file
     data = json.load(f)
 
 for job in data:
     destination = job['Job']['Destination']['File']
+    jobdone = False
 
     # Remap Destination File
     filename = os.path.basename(destination)
     destination = os.path.join('/your/output/path', filename)
-    print(destination)
+    if os.path.exists(destination):
+        jobdone = True
+
+    print('{result}{destination}'.format(
+            result='[' + (concolor['green'] + '✓' + concolor['end'] if jobdone else concolor['red'] + '✗' + concolor['end']) + '] ',
+            destination=destination,
+        ))
