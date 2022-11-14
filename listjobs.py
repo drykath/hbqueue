6590ed52a7a0f6fc02d0582599f04e7c823399e4
Drykath <drykath@drykath.com>
2022-11-14 11:19:43 -0500

    Have listjobs try to indicate jobs in progress

--- listjobs.py
+++ listjobs.py
@@ -5,16 +5,31 @@ import json
 import os
 import sys
 
+concolor = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'blue': '\033[94m',
+        'end': '\033[0m',
+    }
+
+filestates = {
+        'todo': concolor['red'] + '✗' + concolor['end'],
+        'doing': concolor['blue'] + '→' + concolor['end'],
+        'done': concolor['green'] + '✓' + concolor['end'],
+    }
+
+# Files I think might be in progress
+inprogress = []
+
+doinglist = glob.glob('/your/input/path/*/*.*.doing')
+for doingfile in doinglist:
+    with open(doingfile) as f:
+        inprogress.append(f.read())
+
 if len(sys.argv) > 1:
     qlist = [sys.argv[1]]
 else:
     qlist = glob.glob('/your/input/path/*/*.json')
-
-concolor = {
-        'red': '\033[91m',
-        'green': '\033[92m',
-        'end': '\033[0m',
-    }
 
 for qfile in qlist:
     with open(qfile) as f:
@@ -23,17 +38,23 @@ for qfile in qlist:
 
     for job in data:
         destination = job['Job']['Destination']['File']
-        jobdone = False
+        jobstate = 'todo'
 
         # Remap Destination File
         filename = os.path.basename(destination)
         destination = os.path.join('/your/output/path', filename)
         if os.path.exists(destination):
-            jobdone = True
+            jobstate = 'done'
+
+            # But it might actually be in progress
+            source = job['Job']['Source']['Path']
+            localsource = os.path.join(os.path.dirname(qfile), os.path.basename(source))
+            if source in inprogress or localsource in inprogress:
+                jobstate = 'doing'
 
         try:
             print('{result}{destination}'.format(
-                    result='[' + (concolor['green'] + '✓' + concolor['end'] if jobdone else concolor['red'] + '✗' + concolor['end']) + '] ',
+                    result='[' + filestates[jobstate] + '] ',
                     destination=destination,
                 ))
         except (BrokenPipeError, IOError):
